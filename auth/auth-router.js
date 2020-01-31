639ce6d3b63287656dbcde595d2900b2144399c2
Code4Blessings <robinwarden@code4blessings.com>
2020-01-31 12:32:51 -0500

    register and login endpoints tested and authenticated successfully

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,12 +1,43 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
+const bc = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const {jwtSecret} = require('../config/secret');
 
 const Users = require('../users/user-model')
 
+router.get('/register', (req, res) => {
+    if(req.headers.authorization) {
+        bc.hash(req.headers.authorization, 8,(err, hash)=> {
+            if(err) {
+                res.status(500).json({
+                    errorMessage: 'broken code',
+                    message: err.message
+                })
+            }else {
+                res.status(200).json({hash});
+            }
+        });
+    }
+})
+
 router.post('/register', (req, res) => {
-  // implement registration
-});
+    const { username, password } = req.body;
+    const hash = bc.hashSync(password, 10);
+    
+        Users.add({username, password: hash})
+        .then(id => {
+            res.status(201).json({created: id})
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                    errorMessage: "User account could not be created",
+                    message: err.message
+                });
+            });
+        })
+
 
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
@@ -16,7 +47,9 @@ router.post('/login', (req, res) => {
     .then(user => {
       if (user && bc.compareSync(password, user.password)) {
         
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        const token = signToken(user);
+
+        res.status(200).json({ token });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -27,5 +60,20 @@ router.post('/login', (req, res) => {
       });
     });
 });
+
+function signToken(user) {
+  const payload = {
+    userId: user.userId,
+    username: user.username
+  }
+
+  const secret = 'keep it on the down low!'
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
