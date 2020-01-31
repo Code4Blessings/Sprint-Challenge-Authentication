be6fe95f47ff54d596de8a650021202a82a61c76
Code4Blessings <robinwarden@code4blessings.com>
2020-01-31 11:31:07 -0500

    Added users folder and updated model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,31 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const Users = require('../users/user-model')
 
 router.post('/register', (req, res) => {
   // implement registration
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bc.compareSync(password, user.password)) {
+        
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+            errorMessage: err.message
+      });
+    });
 });
 
 module.exports = router;
